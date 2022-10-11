9cb2cdc3e3130383ef2fd309150b8c8bd0e4aba4
ManoelFer <manoelfernandes15@gmail.com>
2022-10-11 13:40:56 -0300

    ✨ Add path mapping and light theme provider

---
+++ src/global/light_theme.ts
@@ -0,0 +1,11 @@
+export const lightTheme = {
+    colors: {
+        common_white: "#FFFFFF",
+    },
+    fonts: ["Lato"],
+    fontSizes: {
+        small: "1em",
+        medium: "2em",
+        large: "3em"
+    }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { ThemeProvider } from 'styled-components';
+import { lightTheme } from '@/global/light_theme'
+
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={lightTheme} >
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
